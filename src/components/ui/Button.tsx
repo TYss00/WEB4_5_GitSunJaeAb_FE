aee1ae8862f2bea749aa7ab45f316e7a78c9a932
KwonTaeHun00 <rnjsxogns0@naver.com>
2025-07-08 10:33:14 +0900

    feat: 버튼 컴포넌트 추가

---
+++ src/components/ui/Button.tsx
@@ -0,0 +1,48 @@
+type ButtonProps = {
+  children: React.ReactNode;
+  buttonStyle?: "green" | "white" | "withIcon" | "smGreen";
+  fullWidth?: boolean;
+  icon?: React.ReactNode;
+  className?: string;
+  onClick?: () => void;
+  type?: "button" | "submit" | "reset";
+};
+
+export default function Button({
+  children,
+  buttonStyle = "green",
+  fullWidth = false,
+  icon,
+  className,
+  onClick,
+  type = "button",
+}: ButtonProps) {
+  const baseStyle =
+    "inline-flex items-center justify-center font-medium transition-all gap-2";
+
+  const styleByType =
+    buttonStyle === "green"
+      ? "bg-[#005C54] text-[#FFFFFF] rounded-[8px]"
+      : buttonStyle === "white"
+      ? "border border-[#005C54] text-[#005C54] bg-[#FFFFFF] rounded-[4px]"
+      : buttonStyle === "smGreen"
+      ? "border border-[#005C54] text-[#FFFFFF] bg-[#005C54] rounded-[4px]"
+      : buttonStyle === "withIcon"
+      ? "border border-[#9F9F9F] text-[#000000] bg-[#FFFFFF] rounded-[8px]"
+      : "";
+
+  const fullWidthStyle = fullWidth ? "w-full" : "";
+
+  return (
+    <button
+      type={type}
+      onClick={onClick}
+      className={`${baseStyle} ${styleByType} ${fullWidthStyle} ${
+        className ?? ""
+      }`}
+    >
+      {icon && <span>{icon}</span>}
+      {children}
+    </button>
+  );
+}
