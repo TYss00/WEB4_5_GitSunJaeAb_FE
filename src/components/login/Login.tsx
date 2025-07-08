555d14e69868a518914a9e547e0c8d8bfe520b3b
TYss00 <rlaxodus203@naver.com>
2025-07-08 12:34:27 +0900

    Feat : 로그인 퍼블리싱

---
+++ src/components/login/Login.tsx
@@ -0,0 +1,62 @@
+'use client';
+
+import { useState } from 'react';
+import { Eye, EyeOff } from 'lucide-react';
+import Image from 'next/image';
+import Input from '../ui/Input';
+
+export default function Login() {
+  const [passwordVisible, setPasswordVisible] = useState(false);
+  const togglePassword = () => setPasswordVisible((prev) => !prev);
+
+  return (
+    <div className="min-h-screen w-full px-6 py-16 flex flex-col justify-center">
+      {/* 로고 */}
+      <h1 className="text-6xl font-bold text-[#005C54] mb-12 text-center">
+        MAPICK
+      </h1>
+
+      {/* 폼 전체 */}
+      <div className="flex flex-col gap-6 w-full max-w-lg mx-auto">
+        {/* 이메일 */}
+        <Input label="이메일" type="email" placeholder="이메일을 입력하세요" />
+
+        {/* 비밀번호 */}
+        <div className="relative">
+          <Input
+            label="비밀번호"
+            type={passwordVisible ? 'text' : 'password'}
+            placeholder="비밀번호를 입력하세요"
+            className="pr-10"
+          />
+          <button
+            type="button"
+            onClick={togglePassword}
+            className="absolute right-3 bottom-3 text-[#005C54]"
+          >
+            {passwordVisible ? <EyeOff size={20} /> : <Eye size={20} />}
+          </button>
+        </div>
+
+        {/* 로그인 버튼 */}
+        <button className="w-full h-[45px] bg-[#005C54] text-white py-2 rounded-lg text-sm">
+          로그인
+        </button>
+
+        {/* 구글 로그인 버튼 */}
+        <button className="w-full h-[45px] border border-gray-300 py-2 rounded-lg text-sm flex items-center justify-center gap-2">
+          <Image src="/assets/google.svg" alt="Google" width={18} height={18} />
+          구글 계정으로 로그인
+        </button>
+
+        {/* 회원가입 안내 */}
+        <p className="text-sm text-center text-black">
+          계정이 없으신가요?{' '}
+          <span className="text-[#005C54] font-semibold cursor-pointer hover:underline">
+            회원가입하기
+          </span>
+        </p>
+      </div>
+    </div>
+  );
+}
