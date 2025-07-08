600b72a19848745d51ec01cd17560201e5447ed6
TYss00 <rlaxodus203@naver.com>
2025-07-08 11:30:17 +0900

    Feat : 헤더 추가

---
+++ src/components/common/Header.tsx
@@ -0,0 +1,46 @@
+'use client';
+
+import { Bell, CircleUserRound, Search } from 'lucide-react'; // 아이콘 라이브러리
+
+export default function Header() {
+  return (
+    <header className="w-full h-20 px-11 flex items-center justify-between bg-white">
+      {/* 로고 */}
+      <div className="text-3xl font-bold text-[#005C54]">MAPICK</div>
+
+      {/* 메뉴들 */}
+      <nav>
+        <ul className="flex items-center gap-[130px] text-lg text-black">
+          <li className="hover:text-[#005C54] transition cursor-pointer">
+            로드맵
+          </li>
+          <li className="hover:text-[#005C54] transition cursor-pointer">
+            공유지도
+          </li>
+          <li className="hover:text-[#005C54] transition cursor-pointer">
+            퀘스트
+          </li>
+        </ul>
+      </nav>
+
+      {/* 아이콘들 */}
+      <div className="flex items-center gap-6 text-black">
+        <Bell
+          size={25}
+          strokeWidth={1.7}
+          className="cursor-pointer hover:text-[#005C54]"
+        />
+        <Search
+          size={25}
+          strokeWidth={1.7}
+          className="cursor-pointer hover:text-[#005C54]"
+        />
+        <CircleUserRound
+          size={25}
+          strokeWidth={1.7}
+          className="cursor-pointer hover:text-[#005C54]"
+        />
+      </div>
+    </header>
+  );
+}
