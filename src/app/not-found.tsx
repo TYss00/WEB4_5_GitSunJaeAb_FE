02789f79cac7bdbdebd51a9a6ed53e036ce6f248
KwonTaeHun00 <rnjsxogns0@naver.com>
2025-07-09 17:46:44 +0900

    Feat: 404페이지 추가

---
+++ src/app/not-found.tsx
@@ -0,0 +1,59 @@
+'use client';
+
+import Button from '@/components/ui/Button';
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+import Lottie from 'react-lottie-player';
+import searchAnimation from '../../public/assets/searchAnimation.json';
+
+export default function NotFound() {
+  const router = useRouter();
+
+  return (
+    <div className="flex items-center justify-center min-h-screen px-6 py-10">
+      <div className="flex flex-col md:flex-row items-center justify-between max-w-[960px] w-full gap-[60px]">
+        <div className="flex flex-col items-start">
+          <span className="text-[16px] text-[#005C54] mb-[16px]">
+            404 error
+          </span>
+          <h1 className="text-[36px] font-bold text-[#222222] mb-[16px]">
+            페이지를 찾을 수 없어요.
+          </h1>
+          <p className="text-[#222222] text-[24px] mb-[50px] leading-relaxed">
+            요청하신 페이지는 존재하지 않거나 <br />
+            다른 위치로 옮겨졌습니다.
+          </p>
+
+          <div className="flex items-center gap-[36px]">
+            <Button
+              buttonStyle="smGreen"
+              onClick={() => router.push('/')}
+              className="w-[162px] h-[52px] text-[18px]"
+            >
+              홈페이지로 이동
+            </Button>
+            <span
+              onClick={() => router.back()}
+              className="text-[18px] text-[#222222]"
+            >
+              이전 페이지로 이동
+            </span>
+          </div>
+        </div>
+
+        <div className="relative w-[500px] h-[500px] shrink-0">
+          <Image
+            src="/assets/404.svg"
+            alt="404"
+            fill
+            className="object-contain z-0"
+          />
+
+          <div className="absolute z-10 bottom-[90px] right-[50px] w-[160px] h-[160px]">
+            <Lottie loop animationData={searchAnimation} play />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
