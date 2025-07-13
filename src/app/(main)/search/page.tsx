499a1f38ec1aa8cf6e7b73ca32eaab959998f8c9
jieun22222 <wldmszz61@gmail.com>
2025-07-13 17:53:16 +0900

    Notification 퍼블리싱 완료

--- src/app/(main)/search/page.tsx
+++ src/app/(main)/search/page.tsx
@@ -1,5 +1,5 @@
 // import SearchModal from '@/components/common/search/SearchModal';
-import SearchResult from '@/components/common/search/SearchResult';
+import SearchResult from '@/components/search/SearchResult';
 
 export default function page() {
   return (
--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -3,19 +3,29 @@
 import { Bell, CircleUserRound, Search, X } from 'lucide-react';
 import Link from 'next/link';
 import { useRef, useState } from 'react';
-import SearchModal from './search/SearchModal';
+import SearchModal from '../search/SearchModal';
 import { useClickOut } from '@/hooks/useClickOut';
+import Notification from '../notification/Notification';
 
 export default function Header() {
   // 검색 아이콘 클릭 시 검색 모달 보여지도록
   const [isSearchOpen, setIsSearchOpen] = useState(false);
-  const modalRef = useRef<HTMLDivElement>(null);
+  const searchRef = useRef<HTMLDivElement>(null);
+
+  // 알림 아이콘 클릭 시 알림 모달 보여지게
+  const [isNotiOpen, setIsNotiOpen] = useState(false);
+  const notiRef = useRef<HTMLDivElement>(null);
 
   // 외부 클릭 감지
-  useClickOut(modalRef, () => setIsSearchOpen(false));
+  useClickOut(searchRef, () => setIsSearchOpen(false));
+  useClickOut(notiRef, () => setIsNotiOpen(false));
 
   const handleSearch = () => {
     setIsSearchOpen((prev) => !prev);
+  };
+
+  const handleNoti = () => {
+    setIsNotiOpen((prev) => !prev);
   };
 
   return (
@@ -49,6 +59,7 @@ export default function Header() {
           size={25}
           strokeWidth={1.7}
           className="cursor-pointer hover:text-[var(--primary-300)]"
+          onClick={handleNoti}
         />
         {/* 검색 아이콘 / 닫기 아이콘 */}
         {isSearchOpen ? (
@@ -75,10 +86,17 @@ export default function Header() {
         </Link>
       </div>
 
+      {/* 알림 모달 추가 */}
+      {isNotiOpen && (
+        <div ref={notiRef} className="absolute top-[80px] right-[80px] z-50">
+          <Notification onClose={() => setIsNotiOpen(false)} />
+        </div>
+      )}
+
       {/* 검색 모달 추가 */}
       {isSearchOpen && (
         <div
-          ref={modalRef}
+          ref={searchRef}
           className="absolute top-[80px] left-0 w-full bg-[var(--white)] shadow-md z-50"
         >
           <SearchModal onClose={() => setIsSearchOpen(false)} />
