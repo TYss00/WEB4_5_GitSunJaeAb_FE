6fa525f9f594b796e7c2c5945eb85a6514353e07
TYss00 <rlaxodus203@naver.com>
2025-07-14 11:09:27 +0900

    Fix : 대문자로변경

--- src/app/(main)/mypage/page.tsx
+++ src/app/(main)/mypage/page.tsx
@@ -1,11 +1,11 @@
 import Mypage from '@/components/mypage/Mypage';
-import Mypageimg from '@/components/mypage/Mypageimg';
-import Mypagelabel from '@/components/mypage/Mypagelabel';
+import MypageImg from '@/components/mypage/MypageImg';
+import Mypagelabel from '@/components/mypage/MypageLabel';
 
 export default async function page() {
   return (
     <>
-      <Mypageimg />
+      <MypageImg />
       <Mypagelabel />
       <Mypage />
     </>
