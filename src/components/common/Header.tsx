24e3fc980f59a91af393b071117c44d29a897dc8
TYss00 <rlaxodus203@naver.com>
2025-07-18 17:29:51 +0900

    Fix : 관리자일때 헤더링크 변경

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -43,7 +43,7 @@ export default function Header({ isAdmin = false }: HeaderProps) {
     <header className="w-full h-20 px-11 flex items-center justify-between bg-[var(--white)] relative">
       {/* 로고 */}
       <Link
-        href="/"
+        href={isAdmin ? '/admin/report' : '/'}
         className="text-3xl text-[var(--primary-300)] font-[vitro-core]"
       >
         MAPICK
@@ -116,7 +116,7 @@ export default function Header({ isAdmin = false }: HeaderProps) {
             />
           </Link>
         )}
-        <Link href="/mypage">
+        <Link href="/">
           <div className="text-xl">로그아웃</div>
         </Link>
       </div>
