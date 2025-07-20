fe60f36a95f7d585fb10ec1662a7b4ceab1f6f12
jieun22222 <wldmszz61@gmail.com>
2025-07-20 16:54:06 +0900

    fix 랜딩페이지 헤더 이동 안되는 버그 수정

--- src/components/landingpage/LandingHeader.tsx
+++ src/components/landingpage/LandingHeader.tsx
@@ -1,28 +1,42 @@
-import Link from 'next/link';
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 export default function LandingHeader() {
+  const router = useRouter();
+
+  const handleRouter = (path: string) => {
+    ScrollTrigger.getAll().forEach((t) => t.kill());
+    router.push(path);
+  };
+
+  const handleScrollTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+
   return (
     <header
       className="w-full h-20 px-11 flex items-center justify-between 
             bg-[var(--white)]/95 backdrop-blur-md fixed z-50"
     >
       {/* 로고 */}
-      <Link
-        href="/"
-        className="text-3xl text-[var(--primary-300)] font-[vitro-core]"
+      <button
+        onClick={handleScrollTop}
+        className="text-3xl text-[var(--primary-300)] font-[vitro-core] cursor-pointer"
       >
         MAPICK
-      </Link>
+      </button>
 
       {/* 로그인 */}
-      <Link
-        href="/login"
+      <button
+        onClick={() => handleRouter('/login')}
         className="text-[18px] font-medium rounded-[20px] px-3.5 py-[5px] 
         border border-[var(--primary-300)] text-[var(--primary-300)]
-        hover:bg-[var(--primary-300)] hover:text-[var(--white)] transition-all"
+        hover:bg-[var(--primary-300)] hover:text-[var(--white)] transition-all cursor-pointer"
       >
         LOGIN
-      </Link>
+      </button>
     </header>
   );
 }
