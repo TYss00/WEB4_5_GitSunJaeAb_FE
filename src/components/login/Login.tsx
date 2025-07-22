0ed9afe3a3c678dc53519691cc82bf7dfaa2a8d4
jieun22222 <wldmszz61@gmail.com>
2025-07-22 15:21:27 +0900

    카테고리 설정

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -53,7 +53,7 @@ export default function Login() {
       alert('로그인 성공');
 
       // 처음 로그인 시 카테고리 설정
-      if (user.loginCount === 0) {
+      if (user.loginCount === 1) {
         router.push('/categories');
       } else {
         // 아니면 메인대시보드로 이동
