aec3681c032cee0c90007caac0dc89242f26bad3
TYss00 <rlaxodus203@naver.com>
2025-07-25 16:00:32 +0900

    Feat : 스켈레톤 추가

--- src/components/mypage/profiletab/AchievementTab.tsx
+++ src/components/mypage/profiletab/AchievementTab.tsx
@@ -5,10 +5,12 @@ import { useEffect, useState } from 'react';
 import axiosInstance from '@/libs/axios';
 import { Achievement } from '@/types/myprofile';
 import { Lock } from 'lucide-react';
+import AchievementSkeleton from '../skeleton/AchievementSkeleton';
 
 export default function AchievementTab() {
   const [achievements, setAchievements] = useState<Achievement[]>([]);
   const [achievedIds, setAchievedIds] = useState<number[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchAchievements = async () => {
@@ -24,6 +26,8 @@ export default function AchievementTab() {
         );
       } catch (err) {
         console.error('업적 불러오기 실패:', err);
+      } finally {
+        setIsLoading(false);
       }
     };
 
@@ -54,45 +58,49 @@ export default function AchievementTab() {
       </div>
 
       <div className="grid grid-cols-3 gap-4 overflow-y-auto pr-1">
-        {achievements
-          .sort((a, b) => {
-            const aAch = achievedIds.includes(a.id);
-            const bAch = achievedIds.includes(b.id);
-            return aAch === bAch ? 0 : aAch ? -1 : 1;
-          })
-          .map((achieve) => {
-            const achieved = achievedIds.includes(achieve.id);
+        {isLoading
+          ? Array.from({ length: 6 }).map((_, idx) => (
+              <AchievementSkeleton key={idx} />
+            ))
+          : achievements
+              .sort((a, b) => {
+                const aAch = achievedIds.includes(a.id);
+                const bAch = achievedIds.includes(b.id);
+                return aAch === bAch ? 0 : aAch ? -1 : 1;
+              })
+              .map((achieve) => {
+                const achieved = achievedIds.includes(achieve.id);
 
-            return (
-              <div
-                key={achieve.id}
-                className={`flex flex-col items-center justify-center p-5 rounded-xl border text-center transition-all duration-200 bg-white border-[var(--gray-100)] ${
-                  !achieved ? 'opacity-40' : ''
-                }`}
-              >
-                <div className="w-24 h-24 rounded-full overflow-hidden relative mb-3 flex items-center justify-center bg-[var(--gray-100)]">
-                  {achieved ? (
-                    <Image
-                      src={
-                        achieve.image?.trim()
-                          ? achieve.image
-                          : '/이미지없다.jpg'
-                      }
-                      alt={achieve.name}
-                      fill
-                      sizes="96px"
-                      className="object-cover"
-                    />
-                  ) : (
-                    <Lock className="w-8 h-8 text-[var(--gray-400)]" />
-                  )}
-                </div>
-                <span className="text-sm font-medium text-[var(--black)]">
-                  {achieve.name}
-                </span>
-              </div>
-            );
-          })}
+                return (
+                  <div
+                    key={achieve.id}
+                    className={`flex flex-col items-center justify-center p-5 rounded-xl border text-center transition-all duration-200 bg-white border-[var(--gray-100)] ${
+                      !achieved ? 'opacity-40' : ''
+                    }`}
+                  >
+                    <div className="w-24 h-24 rounded-full overflow-hidden relative mb-3 flex items-center justify-center bg-[var(--gray-100)]">
+                      {achieved ? (
+                        <Image
+                          src={
+                            achieve.image?.trim()
+                              ? achieve.image
+                              : '/이미지없다.jpg'
+                          }
+                          alt={achieve.name}
+                          fill
+                          sizes="96px"
+                          className="object-cover"
+                        />
+                      ) : (
+                        <Lock className="w-8 h-8 text-[var(--gray-400)]" />
+                      )}
+                    </div>
+                    <span className="text-sm font-medium text-[var(--black)]">
+                      {achieve.name}
+                    </span>
+                  </div>
+                );
+              })}
       </div>
     </div>
   );
